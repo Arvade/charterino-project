3a07a2fa7c06e3dff287a6c96e6f20dd337f0e2b
Pakietrejser <bartosz.ptak98@gmail.com>
2019-11-17 20:39:09 +0100

    added dataMock

--- charterino/charterino/VisualizeDataPage.xaml.cs
+++ charterino/charterino/VisualizeDataPage.xaml.cs
@@ -21,10 +21,17 @@ namespace charterino
     /// </summary>
     public partial class VisualizeDataPage : Page
     {
-        List<Product> data = new List<Product>();
+        List<Product> dataMock = new List<Product>();
 
         public VisualizeDataPage()
         {
+            dataMock.Add(new Product(1, "Cool product1", 2.52, 10));
+            dataMock.Add(new Product(2, "Cool product2", 3.52, 12));
+            dataMock.Add(new Product(3, "Cool product3", 5.52, 15));
+            dataMock.Add(new Product(4, "Cool product4", 1.30, 156));
+            dataMock.Add(new Product(5, "Cool product5", 9.30, 11));
+            dataMock.Add(new Product(6, "Cool product6", 1.92, 19));
+
             InitializeComponent();
         }
     }
