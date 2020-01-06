f55e036cb3fb2d41990a6fda2113a0aa98ddee17
Arvade <55897734+Arvade@users.noreply.github.com>
2020-01-06 18:42:13 +0100

    FileImportData impl

---
+++ charterino-bo/charterino_bo/file-import/CompositeProductFileImport.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using charterino_bo.model;
+
+namespace charterino_bo.import {
+    public class CompositeProductFileImport : FileImportData<Product> {
+        private List<FileImportData<Product>> strategies = new List<FileImportData<Product>>();
+
+        public CompositeProductFileImport(List<FileImportData<Product>> strategies) {
+            this.strategies = strategies;
+        }
+
+        public List<Product> Import(StreamReader streamReader) {
+            List<FileImportData<Product>> supportedStrategies =
+                strategies.Where(strategy => strategy.supports(streamReader)).ToList();
+            if (supportedStrategies.Count == 0) {
+                throw new ArgumentException("No strategy found for " + streamReader);
+            }
+
+            return supportedStrategies[0].Import(streamReader);
+        }
+
+        public bool supports(StreamReader streamReader) {
+            return strategies.Where(strategy => strategy.supports(streamReader)).ToList().Count > 0;
+        }
+
+        public static CompositeProductFileImportBuilder Builder() {
+            return new CompositeProductFileImportBuilder();
+        }
+
+        public class CompositeProductFileImportBuilder {
+            private List<FileImportData<Product>> strategies = new List<FileImportData<Product>>();
+
+            public CompositeProductFileImportBuilder registerStrategy(FileImportData<Product> strategy) {
+                strategies.Add(strategy);
+                return this;
+            }
+
+            public CompositeProductFileImport build() {
+                return new CompositeProductFileImport(strategies);
+            }
+        }
+    }
+}
---
+++ charterino-bo/charterino_bo/file-import/JsonFileImport.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using charterino_bo.model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace charterino_bo.import {
+    public class JsonFileImport : FileImportData<Product> {
+        private const string SUPPORTED_EXTENSION = "json";
+
+        public List<Product> Import(StreamReader streamReader) {
+            using (JsonTextReader reader = new JsonTextReader(streamReader)) {
+                JToken token = JToken.ReadFrom(reader);
+                if (token.Type == JTokenType.Array) {
+                    return token.ToObject<List<Product>>();
+                }
+
+                Product data = token.ToObject<Product>();
+                return new List<Product>(new[] {data});
+            }
+        }
+
+        public bool supports(StreamReader streamReader) {
+            string path = (streamReader.BaseStream as FileStream)?.Name;
+            string[] pathParts = path.Split('/');
+            string[] filenameParts = pathParts[pathParts.Length - 1].Split('.');
+            if (filenameParts.Length > 1) {
+                return filenameParts[1] == SUPPORTED_EXTENSION;
+            }
+
+            return false;
+        }
+    }
+}
