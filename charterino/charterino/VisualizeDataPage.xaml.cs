dbb74fec8e9a8631fe2786b68140c7368d8be098
Pakietrejser <bartosz.ptak98@gmail.com>
2020-01-21 20:16:13 +0100

    BO updated with Charts

--- charterino/charterino/VisualizeDataPage.xaml.cs
+++ charterino/charterino/VisualizeDataPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using charterino_bo.model;
+using charterino_bo.chart;
 using LiveCharts.Wpf;
 using LiveCharts;
 using System;
@@ -24,28 +25,25 @@ namespace charterino
     /// </summary>
     public partial class VisualizeDataPage : Page
     {
-        List<Product> dataMock = new List<Product>();
-
         public Func<ChartPoint, string> PiePointLabel { get; set; }
-
-        //Pie Chart
         public SeriesCollection PieChartCollection { get; set; }
-
-        //Bar Chart
         public SeriesCollection BarChartCollection { get; set; }
-        public string[] BarLabels { get; set; }
-        public Func<int, string> BarFormatter { get; set; }
-
-        //Stacked Chart
+        public string[] BarLabels;
+        public Func<int, string> BarFormatter;
         public SeriesCollection StackedChartCollection { get; set; }
-        public Func<double, string> StackedFormatter { get; set; }
+        public Func<double, string> StackedFormatter;
 
         string[] distinctCategories;
+        readonly SolidColorBrush lightGreen = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF389484");
+        readonly SolidColorBrush darkGreen = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF246459");
+
+        Charts Charts;
+        //TODO: remove
+        List<Product> dataMock = new List<Product>();
 
         public VisualizeDataPage()
         {
-            InitializeComponent();
-
+            //TODO: remove
             dataMock.Add(new Product(1, "Rudy", "humans", 2.52, 10));
             dataMock.Add(new Product(2, "Gothic 5", "games", 3.52, 12));
             dataMock.Add(new Product(3, "Badocha", "humans", 5.52, 15));
@@ -59,123 +57,71 @@ namespace charterino
             dataMock.Add(new Product(5, "Szklanka", "accessories", 9.30, 11));
             dataMock.Add(new Product(6, "Pepsi", "drinks", 1.92, 19));
 
-            distinctCategories = dataMock.Select(product => product.category).Distinct().ToArray();
+            InitializeComponent();
 
-            CreateBarChart();
-            CreatePieChart();
-            CreateStackedChart();
+            //TODO: move to main window (from B.Ptak to S.M.)
+            Charts = new Charts();
 
-            ChartBar.Visibility = Visibility.Hidden;
-            ChartPie.Visibility = Visibility.Hidden;
-            ChartStacked.Visibility = Visibility.Hidden;
-
-            ShowBarChart();
+            distinctCategories = Charts.DistinctCategories(dataMock);
 
             ChartBarButton.Click += (s, e) => ShowBarChart();
             ChartStackedButton.Click += (s, e) => ShowStackedChart();
             ChartPieButton.Click += (s, e) => ShowPieChart();
+
+            InstantiateBarChart();
+            InstantiatePieChart();
+            InstantiateStackedChart();
+
+            ShowBarChart();
         }
 
-        public void CreateBarChart()
+        void InstantiateBarChart()
         {
-            BarChartCollection = new SeriesCollection();
-
-            for (int i = 0; i < dataMock.Count; i++)
-            {
-                BarChartCollection.Add(new StackedColumnSeries
-                {
-                    Title = dataMock[i].name,
-                    Values = new ChartValues<int> {},
-                });
-                int index = Array.FindIndex(distinctCategories, x => x == dataMock[i].category);
-                for (int j = 0; j < index+1; j++)
-                {
-                    if (j == index)
-                        BarChartCollection[i].Values.Add(dataMock[i].sold);
-                    else
-                        BarChartCollection[i].Values.Add(0);
-                }
-            }
-
-            BarLabels = dataMock.Select(product => product.category).Distinct().ToArray();
-            BarFormatter = value => value.ToString();
+            BarChartCollection = Charts.GenerateBarChart(dataMock, distinctCategories, out BarLabels, out BarFormatter);
+            DataContext = this;
+        }
+        void InstantiatePieChart()
+        {
+            PieChartCollection = Charts.GeneratePieChart(dataMock, distinctCategories);
+            DataContext = this;
+        }
+        void InstantiateStackedChart()
+        {
+            StackedChartCollection = Charts.GenerateStackedChart(dataMock, distinctCategories, out StackedFormatter);
             DataContext = this;
         }
 
-        public void CreateStackedChart()
+        void ShowBarChart()
         {
-            StackedChartCollection = new SeriesCollection();
-
-            for (int i = 0; i < distinctCategories.Length; i++)
-            {
-                double income = 0;
-
-                foreach (var product in dataMock)
-                {
-                    if (product.category == distinctCategories[i])
-                        income += dataMock[i].sold * dataMock[i].price;
-                }
-
-                StackedChartCollection.Add(new RowSeries 
-                { 
-                    Title = distinctCategories[i],
-                    Values = new ChartValues<double>
-                    {
-                        income
-                    },
-                });
-            }
-
-            StackedFormatter = val => val.ToString("N") + " $";
-
-            DataContext = this;
-        }
-
-        public void CreatePieChart()
-        {
-            PieChartCollection = new SeriesCollection();
-
-            for (int i = 0; i < distinctCategories.Length; i++)
-            {
-                int productAmount = 0;
-
-                foreach (var product in dataMock)
-                {
-                    if (product.category == distinctCategories[i])
-                        productAmount++;
-                }
-
-                PieChartCollection.Add(new PieSeries
-                {
-                    Title = productAmount + " different " + distinctCategories[i],
-                    Values = new ChartValues<int> {
-                        productAmount
-                    }
-                });
-            }
-
-            DataContext = this;
-        }
-
-        public void ShowBarChart()
-        {
-            ChartBar.Visibility = Visibility.Visible;
             ChartPie.Visibility = Visibility.Hidden;
             ChartStacked.Visibility = Visibility.Hidden;
+            ChartBar.Visibility = Visibility.Visible;
+            ChangeButtonColor(ChartBarButton);
         }
 
-        public void ShowPieChart()
+        void ShowPieChart()
         {
             ChartBar.Visibility = Visibility.Hidden;
-            ChartPie.Visibility = Visibility.Visible;
             ChartStacked.Visibility = Visibility.Hidden;
+            ChartPie.Visibility = Visibility.Visible;
+            ChangeButtonColor(ChartPieButton);
         }
 
-        public void ShowStackedChart()
+        void ShowStackedChart()
         {
             ChartBar.Visibility = Visibility.Hidden;
             ChartPie.Visibility = Visibility.Hidden;
             ChartStacked.Visibility = Visibility.Visible;
+            ChangeButtonColor(ChartStackedButton);
+        }
+
+        public void ChangeButtonColor(Button button)
+        {
+            ChartBarButton.Background = lightGreen;
+            ChartStackedButton.Background = lightGreen;
+            ChartPieButton.Background = lightGreen;
+
+            button.Background = darkGreen;
         }
     }
 }
